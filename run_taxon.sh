bd2bcc1d0001c4e6681cb644eb71fe8e1a60cc7c
Hayden Metsky <hmetsky@gmail.com>
2019-04-11 17:34:33 -0400

    Only perform cover-by-year-decay for influenza

--- run_taxon.sh
+++ run_taxon.sh
@@ -21,15 +21,15 @@ ARG_MAXPRIMERSATSITE="5"
 ARG_MAXTARGETLENGTH="500"
 ARG_COSTFNWEIGHTS="0.6667 0.2222 0.1111"
 ARG_BESTNTARGETS="15"
-ARG_COVERBYYEARSTART=2015
-ARG_COVERBYYEARDECAY=0.95
 
 # Make the memoize directory
 mkdir -p /tmp/prep-memoize-dir
 
 # Set --prep-influenza if needed
 if [ "$taxid" == "11320" ] || [ "$taxid" == "11520" ]; then
-    ARGS_INFLUENZA="--prep-influenza"
+    ARG_COVERBYYEARSTART=2015
+    ARG_COVERBYYEARDECAY=0.95
+    ARGS_INFLUENZA="--prep-influenza --cover-by-year-decay $ARG_COVERBYYEARSTART $ARG_COVERBYYEARDECAY"
 else
     ARGS_INFLUENZA=""
 fi
@@ -44,7 +44,7 @@ outdir="out/designs/${taxid}_${segment}/${timestamp}"
 mkdir -p $outdir
 
 # Run the design
-design.py complete-targets auto-from-args $taxid $segment $refaccs $outdir/design.tsv -gl $ARG_GL -gm $ARG_GM -gp $ARG_GP -pl $ARG_PL -pm $ARG_PM -pp $ARG_PP --max-primers-at-site $ARG_MAXPRIMERSATSITE --max-target-length $ARG_MAXTARGETLENGTH --cost-fn-weights $ARG_COSTFNWEIGHTS --best-n-targets $ARG_BESTNTARGETS --mafft-path $MAFFT_PATH --prep-memoize-dir $PREP_MEMOIZE_DIR --cluster-threshold $CLUSTER_THRESHOLD --cover-by-year-decay $ARG_COVERBYYEARSTART $ARG_COVERBYYEARDECAY $ARGS_INFLUENZA --verbose &> $outdir/design.out
+design.py complete-targets auto-from-args $taxid $segment $refaccs $outdir/design.tsv -gl $ARG_GL -gm $ARG_GM -gp $ARG_GP -pl $ARG_PL -pm $ARG_PM -pp $ARG_PP --max-primers-at-site $ARG_MAXPRIMERSATSITE --max-target-length $ARG_MAXTARGETLENGTH --cost-fn-weights $ARG_COSTFNWEIGHTS --best-n-targets $ARG_BESTNTARGETS --mafft-path $MAFFT_PATH --prep-memoize-dir $PREP_MEMOIZE_DIR --cluster-threshold $CLUSTER_THRESHOLD $ARGS_INFLUENZA --verbose &> $outdir/design.out
 
 # gzip the stdout/stderr
 gzip $outdir/design.out
